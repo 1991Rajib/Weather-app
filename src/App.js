60d0badfdc84db95cf19e1d4dfbb1efc8a4bca1f
Rajib Chatterjee <rajib.etc1991@gmail.com>
2023-10-13 00:40:33 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,104 @@
+import './App.css';
+import React, { useEffect, useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import WeatherBody from './component/WeatherBody';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  // const [lat, setLat] = useState(null);
+  // const [long, setLong] = useState(null);
+  const [error, setError] = useState("");
+  const [data, setData] = useState(null);
+  const [city, setCity] = useState({});
+
+  const setFormValue = (e) => {
+    setCity({ ...city, [e.target.name]: e.target.value });
+    console.log(city);
+  };
+
+
+  // useEffect(() => {
+  //   let longitude;
+  //   let latitude;
+  //   navigator.geolocation.getCurrentPosition(function (position) {
+  //     latitude = position.coords.latitude
+  //     longitude = position.coords.longitude
+  //     setLat(latitude);
+  //     setLong(longitude);
+  //   });
+  // }, [])
+
+  // useEffect(() => {
+  //   const getWeather = async () => {
+  //     try {
+  //       //const response = await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}&units=metric`)
+  //       const response = await fetch(`${process.env.REACT_APP_API_URL}/weather/?q=${city}&APPID=${process.env.REACT_APP_API_KEY}&units=metric`)
+  //       const data = await response.json();
+  //       console.log(data);
+  //       setData(data);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   }
+  //   if (lat && long) { getWeather() }
+
+  // }, [lat, long]);
+
+
+
+  const handleCity = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/weather/?q=${city.city}&APPID=${process.env.REACT_APP_API_KEY}&units=metric`)
+      if (response.status !== 404) {
+        const data = await response.json();
+        console.log(data);
+        setData(data);
+        setError("");
+      }
+      else {
+        setData(null);
+        setError("No city found. Please choose another one");
+      }
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+  return (
+    <section className="vh-100">
+      <div className="container py-5">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col-md-9 col-lg-7 col-xl-5">
+            <form onSubmit={handleCity} className='mb-4'>
+              <Form.Label htmlFor="city">City</Form.Label>
+              <div className='d-flex'>
+                <Form.Control
+                  name='city'
+                  type="text"
+                  id="city"
+                  aria-describedby="city"
+                  onChange={setFormValue}
+                  className='rounded-0'
+                />
+                <Button className='rounded-0' variant="primary" type="submit">
+                  Submit
+                </Button>
+              </div>
+              <Form.Text id="passwordHelpBlock" muted>
+                Please enter city name
+              </Form.Text>
+            </form>
+            {data && (<><WeatherBody data={data} /></>)}
+            <p>{error}</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default App;
---
+++ src/component/WeatherBody.jsx
@@ -0,0 +1,132 @@
+import React, { useEffect, useState } from 'react'
+
+const WeatherBody = ({ data }) => {
+
+  const[wdata,setWdata] = useState({})
+  let obj ={}
+  const weatherData = () => {
+    data.weather.forEach((item) => {
+      obj = {
+        description: item.description,
+        icon: item.icon,
+        main: item.main
+      }
+    })
+    setWdata(obj)
+  }
+
+  useEffect(() => {
+    weatherData()
+  },[])
+
+  return (
+
+
+            <div id="wrapper-bg" className="card text-white bg-image shadow-4-strong">
+
+                  {/* <!-- Main current data --> */}
+              <div className="card-header p-4 border-0">
+                <div className="text-center mb-3">
+                  <p className="h2 mb-1" id="wrapper-name">{data.name}</p>
+                  <p className="display-1 mb-1" id="wrapper-temp">{data.main?.temp}<sup><small>.c</small></sup></p>
+                  <p className="mb-1" id="wrapper-description"><img src={`https://openweathermap.org/img/wn/${wdata?.icon}.png`} alt=""/>{wdata?.description}</p>
+                  <span className="">Feels Like: <span id="wrapper-pressure">{data.main?.feels_like}<sup><small>.c</small></sup></span></span>
+                  <span className="mx-2">|</span>
+                  <span className="">Pressure: <span id="wrapper-pressure">{data.main?.pressure}</span></span>
+                  <span className="mx-2">|</span>
+                  <span className="">Humidity: <span id="wrapper-humidity">{data.main?.humidity}</span></span>
+                </div>
+              </div>
+
+              {/* <!-- Hourly forecast --> */}
+              {/* <div className="card-body p-4 border-top border-bottom mb-2">
+                <div className="row text-center">
+                  <div className="col-2">
+                    <strong className="d-block mb-2">Now</strong>
+                    <img id="wrapper-icon-hour-now" src="" className="" alt="" />
+                    <strong className="d-block" id="wrapper-hour-now"></strong>
+                  </div>
+
+                  <div className="col-2">
+                    <strong className="d-block mb-2" id="wrapper-time1"></strong>
+                    <img id="wrapper-icon-hour1" src="" className="" alt="" />
+                    <strong className="d-block" id="wrapper-hour1"></strong>
+                  </div>
+
+                  <div className="col-2">
+                    <strong className="d-block mb-2" id="wrapper-time2"></strong>
+                    <img id="wrapper-icon-hour2" src="" className="" alt="" />
+                    <strong className="d-block" id="wrapper-hour2"></strong>
+                  </div>
+
+                  <div className="col-2">
+                    <strong className="d-block mb-2" id="wrapper-time3"></strong>
+                    <img id="wrapper-icon-hour3" src="" className="" alt="" />
+                    <strong className="d-block" id="wrapper-hour3"></strong>
+                  </div>
+
+                  <div className="col-2">
+                    <strong className="d-block mb-2" id="wrapper-time4"></strong>
+                    <img id="wrapper-icon-hour4" src="" className="" alt="" />
+                    <strong className="d-block" id="wrapper-hour4"></strong>
+                  </div>
+
+                  <div className="col-2">
+                    <strong className="d-block mb-2" id="wrapper-time5"></strong>
+                    <img id="wrapper-icon-hour5" src="" className="" alt="" />
+                    <strong className="d-block" id="wrapper-hour5"></strong>
+                  </div>
+                </div>
+              </div> */}
+
+              {/* <!-- Daily forecast --> */}
+              {/* <div className="card-body px-5">
+                <div className="row align-items-center">
+                  <div className="col-lg-6">
+                    <strong>Today</strong>
+                  </div>
+
+                  <div className="col-lg-2 text-center">
+                    <img id="wrapper-icon-today" src="" className="w-100" alt="" />
+                  </div>
+
+                  <div className="col-lg-4 text-end">
+                    <span id="wrapper-forecast-temp-today"></span>
+                  </div>
+                </div>
+
+                <div className="row align-items-center">
+                  <div className="col-lg-6">
+                    <strong>Tomorrow</strong>
+                  </div>
+
+                  <div className="col-lg-2 text-center">
+                    <img id="wrapper-icon-tomorrow" src="" className="w-100" alt="" />
+                  </div>
+
+                  <div className="col-lg-4 text-end">
+                    <span id="wrapper-forecast-temp-tomorrow">28</span>
+                  </div>
+                </div>
+
+                <div className="row align-items-center">
+                  <div className="col-lg-6">
+                    <strong>Day after tomorrow</strong>
+                  </div>
+
+                  <div className="col-lg-2 text-center">
+                    <img id="wrapper-icon-dAT" src="" className="w-100" alt="" />
+                  </div>
+
+                  <div className="col-lg-4 text-end">
+                    <span id="wrapper-forecast-temp-dAT">28</span>
+                  </div>
+                </div>
+              </div> */}
+            </div>
+
+
+  )
+}
+
+export default WeatherBody
