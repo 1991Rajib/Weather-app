91dc98d68f1723c36bdc65d3c6bca4cfe44091f2
system1 <team@codeclouds.biz>
2023-10-13 17:03:24 +0530

    wip

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import WeatherBody from './component/WeatherBody';
 import Form from 'react-bootstrap/Form';
